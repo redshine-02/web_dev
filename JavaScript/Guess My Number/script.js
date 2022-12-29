2045846c9b744953596366a40cedc628e322a3db
PALAK SINGH <80193531+redshine-02@users.noreply.github.com>
2022-12-29 11:52:56 +0530

    updates
    9th done

--- JavaScript/Guess My Number/script.js
+++ JavaScript/Guess My Number/script.js
@@ -31,15 +31,72 @@ So DOM is automatically provided by web browser APIs
 //              Working on our real application
 
 // Firstly we would be using listeners ==> what I can recall , listener basically listens the particular element ist is attached to
-let a = 12;
-document.querySelector('.check').addEventListener('click', function () {
-  // Here the function is the event handler
-  while (document.querySelector('.guess').value != a) {
-    console.log(document.querySelector('.guess').value);
-    document.querySelector('.label-score').value--;
+/**
+ * What are the steps I need to follow?
+ * 1. Get the guessing number
+ * 2. Check for the click on the check button => if not entered anything then change the message to Enter something
+ * 3. Take the number as given or entered 
+ * 4. If(given===secret) ==> change the message to "You Won"
+ * 5. If(given>secret) ==> change the message to "Too High"
+ * 6. If(given<secret) ==> change the message to "Too Low"
+ * 
+ * 7. I need to add the score functionality
+ */
+
+//Step 1
+let secret = Math.trunc(Math.random()*20)+1;
+//console.log(secret,typeof secret);
+//document.querySelector('.number').textContent=secret;
+let max = 0 ;
+function displayMessage(message){
+  document.querySelector('.message').textContent=message;
+}
+
+document.querySelector('.check').addEventListener('click',function(){
+  //Step 3
+  let ss = Number(document.querySelector('.score').textContent);
+  console.log('************'+ss);
+  let given = Number(document.querySelector('.guess').value);
+  console.log(given,typeof given);
+  //Step2
+  if(given===0){
+    //document.querySelector('.message').textContent="Enter Something";
+    displayMessage("Enter Something");
   }
-  console.log(document.querySelector('.guess').value);
-  document.querySelector('.label-highscore').value =
-    document.querySelector('.label-score').value;
-  document.querySelector('.message').textContent = 'Correct Answer !!' + a;
+  //Step 4
+  else if(given===secret){
+    //document.querySelector('.message').textContent="You Won";
+    displayMessage("You Won");
+    max = Math.max(max,ss);
+    document.querySelector('.number').textContent=secret;
+    document.querySelector('.highscore').textContent=max;
+    document.querySelector('body').style.backgroundColor= '#60b347';
+    document.querySelector('.number').style.width='30rem';
+  }
+  //Step 5 & Step 6
+  else{
+    if(ss>0){
+    //document.querySelector('.message').textContent=given>secret?"Too High":"Too Low";
+    displayMessage(given>secret?"Too High":"Too Low");
+    ss--;
+    }
+    else{
+    //document.querySelector('.message').textContent="You Lost";
+    displayMessage("You Lost");
+    document.querySelector('body').style.backgroundColor= 'rgb(255,0,0';
+
+    }
+  }
+  document.querySelector('.score').textContent=ss;
 });
+
+document.querySelector('.again').addEventListener('click',function(){
+    secret = Math.trunc(Math.random()*20)+1;
+    //document.querySelector('.message').textContent='... Start guessing';
+    displayMessage('... Start guessing');
+    document.querySelector('.score').textContent=20;
+    document.querySelector('body').style.backgroundColor= 'black';
+    document.querySelector('.number').textContent='?';
+    document.querySelector('.guess').value='';
+    document.querySelector('.number').style.width='15rem';
+})
